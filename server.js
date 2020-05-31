59a6a9d142cac2e593e83acc8ddfe12834e40a87
Suyank Saraswat <suyank.saraswat@gmail.com>
2020-06-01 00:22:38 +0530

    add user model and connect to db

--- server.js
+++ server.js
@@ -4,27 +4,30 @@ const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
 
+const User=require('./models/User');
+mongoose.connect('mongodb://localhost/userData');
+
 app.use(bodyParser.json());
 
 app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
-})
+	console.log(`server is listening on port:${port}`);
+});
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
-})
+	// User.create()
+});
 
 app.route('/users/:id')
 // READ
-.get((req,res)=>{
-  // User.findById()
-})
+	.get((req,res)=>{
+		// User.findById()
+	})
 // UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
+	.put((req,res)=>{
+		// User.findByIdAndUpdate()
+	})
 // DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+	.delete((req,res)=>{
+		// User.findByIdAndDelete()
+	});
